f7cfc5f46afd2cad38507f8acd0e2478dcba9145
joonsol <71648638+joonsol@users.noreply.github.com>
2025-10-30 10:03:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const port = process.env.PORT
 
 
 app.get('/', (req, res) => {
@@ -8,5 +9,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+  console.log(`Example app listening on port ${PORT}`)
 })

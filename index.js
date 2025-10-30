2903cc75ab12f2c05dc75a1248dc017fd675ff88
joonsol <71648638+joonsol@users.noreply.github.com>
2025-10-30 10:10:46 +0900

    Refactor to use DATABASE_NAME from environment variables

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT
-
+const stu_name=process.env.DATABASE_NAME
 
 app.get('/', (req, res) => {
-  res.send(`.env 테스트 : ${process.env.DATABASE_NAME}`);
+  res.send(`.env 테스트 : ${stu_name}`);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`Example app listening on port ${port} : 학생이름:$${stu_name}`)
 })
